81e81008f70cdb4df78c31fe431acb0ac4527ecd
ThiagoDeus2512 <Thiagodedeusjacom.td1@gmail.com>
2024-08-21 07:23:56 -0300

    Aulalogica

---
+++ funcoes.js
@@ -0,0 +1,19 @@
+class Pessoa {
+
+    nome;
+    peso;
+    alt;
+
+    constructor(nome, peso, alt) {
+        this.nome = nome;
+        this.peso = peso;
+        this.alt = alt;
+
+    }
+    calcularImc() {
+        return this.peso / (this.alt * this.alt);
+    }
+
+
+
+}
